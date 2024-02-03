286ba2778863ad748713379b525d6ce0726382bc
Dantesos33 <dantesos33@gmail.com>
2024-02-03 16:40:01 +0500

    Animations fixed, New media queries for responsive design, overflow issue fixed.

--- app.js
+++ app.js
@@ -6,26 +6,37 @@ const observer1 = new IntersectionObserver((entries) => {
 })
 
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer1.observe(el));
+hiddenElements.forEach((el1) => observer1.observe(el1));
 
 // Right Elements
 
 const observer2 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-            entry.target.classList.toggle('not-right',entry.isIntersecting);
+        if(entry.isIntersecting){
+            entry.target.classList.add('not-right');
+        }
+        else{
+            entry.target.classList.remove('not-right');
+        }
     })
 })
 
 const rightElements = document.querySelectorAll('.right');
-rightElements.forEach((el) => observer2.observe(el));
+rightElements.forEach((el2) => observer2.observe(el2));
 
 // Reveal Elements
 
 const observer3 = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-            entry.target.classList.toggle('un-reveal',entry.isIntersecting);
+        if(entry.isIntersecting){
+            entry.target.classList.add('un-reveal');
+        }
+        else{
+            entry.target.classList.remove('un-reveal');
+        }
     })
-})
+},
+{threshold: 0.3})
 
 const revealElements = document.querySelectorAll('.reveal');
-revealElements.forEach((el) => observer3.observe(el));
+revealElements.forEach((el3) => observer3.observe(el3));
